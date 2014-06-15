f14bbfdb6e2a485c69f73f0aa8c4419811200599
Myf Ma <myf@worldtalks.org>
2014-06-15 12:40:42 -0400

    jschneier's suggestion on request.json

--- songza.py
+++ songza.py
@@ -3,10 +3,6 @@ from optparse import OptionParser
 import subprocess
 import requests
 import sys
-try:
-    import simplejson as json
-except:
-    import json
 USER_AGENT = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36'
 
 def play_station(num_code, cookies=None, proxy=None, download=None):
@@ -24,20 +20,20 @@ def play_station(num_code, cookies=None, proxy=None, download=None):
     else:
         response = requests.get(url, headers=headers, proxies=proxies)
         cookies = response.cookies
-    response_json = json.loads(response.text)
+    response_json = response.json()
     listen_url = response_json['listen_url']
     song = response_json['song']
 
     print "song is %s by %s" % (song[u'title'], song[u'artist'][u'name'])
     cookie_str = ":".join(map(str, cookies.items()[0]))
     #call(["/usr/bin/mplayer", '-cookies', cookie_str, '-really-quiet', listen_url])
-    #call(["/usr/bin/mplayer", '-really-quiet', listen_url])
+    subprocess.call(["/usr/bin/mplayer", '-really-quiet', listen_url])
     #import ipdb;ipdb.set_trace()
     #curl = subprocess.call(["/usr/bin/curl", listen_url], stdout=subprocess.PIPE)
     #mplayer = subprocess.call(["/usr/bin/mplayer", '-really-quiet', "-"], stdin=curl.stdout, stdout=subprocess.PIPE)
     #mplayer.communicate()[0]
 
-    subprocess.call(['/usr/bin/wget', listen_url])
+    #subprocess.call(['/usr/bin/wget', listen_url])
     play_station(num_code, cookies=cookies, proxy=proxy, download=download)
 
 if __name__ == "__main__":
